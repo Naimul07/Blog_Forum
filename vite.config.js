fcfe65e629447608526548f57c1fe9c4126552ba
Naimul07 <naimul.cse.brur@gmail.com>
2024-10-14 15:41:58 +0600

    api path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      '/Api':{
+        target:'http://127.0.0.1:8000/api',
+        changeOrigin:true,
+        rewrite:(path)=>path.replace(/^\/Api/,''),
+      },
+    },
+  },
 })
