f8bee6f2594ff6338135d31bccfe2c587a76474e
Naimul07 <naimul.cse.brur@gmail.com>
2024-10-16 15:15:57 +0600

    Updated protected route

--- src/Layout/Protected.jsx
+++ src/Layout/Protected.jsx
@@ -1,10 +1,18 @@
-import { Navigate,Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom";
+import useAuthStore from "../Store/AuthStore"; // Import the Zustand store
 
 function Protected() {
-    const auth = localStorage.getItem('userToken')
-  return auth ? <Outlet/> : <Navigate to= "/login"/>
+  // Get the token and email verification status from Zustand store
+  const token = useAuthStore((state) => state.token);
+  const emailVerified = useAuthStore((state) => state.email_verified_at);
+
+  // Check if user is authenticated but email is not verified
+  if (token && !emailVerified) {
+    return <Navigate to="/email_verification" />;
+  }
+
+  // If token is present and email is verified, allow access to the route
+  return token && emailVerified ? <Outlet /> : <Navigate to="/login" />;
 }
 
-
-
-export default Protected
+export default Protected;
