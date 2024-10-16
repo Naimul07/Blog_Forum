94734e8b5f91be0c2e1e43a95622f88ec5f98bdc
Naimul07 <naimul.cse.brur@gmail.com>
2024-10-16 15:39:13 +0600

    logout functionality implemented

--- src/Components/LogOut.jsx
+++ src/Components/LogOut.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import axios from "axios";
+import useAuthStore from "../Store/AuthStore"
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 
 function LogOut() {
+  const token = useAuthStore((state) => state.token);
+  const clearAuth = useAuthStore((state)=>state.clearAuth);
+  const navigate = useNavigate();
+  async function handleClick() {
+    try {
+      const response = await axios.post('/Api/logout',{
+        headers:{
+          Authorization:`Bearer ${token}`,
+        }
+      });
+      clearAuth();
+      toast.success(response.data.message);
+      navigate('/');
+    } catch (err) {
+      toast.error(err.response.data.message);
+    } 
+
+  }
   return (
-    <div>LogOut</div>
+    <>
+      <button onClick={handleClick}>Logout</button>
+    </>
   )
 }
 
