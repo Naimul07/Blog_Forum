0aeea2b92dfddc0e3f7246e84985aab2f02d0d96
Naimul07 <naimul.cse.brur@gmail.com>
2024-10-17 15:05:06 +0600

    added structure to sidebar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,19 @@
+import SideBar from "../Components/SideBar"
 
 
 function HomePage() {
   return (
-    <div className="pt-10">HomePage</div>
+    <>
+      <div className="pt-14">
+        <div className="grid grid-cols-1 sm:grid-cols-4">
+          <div className="sm:block border">
+            <SideBar/>
+          </div>
+          <div className="col-span-2 px-10 border">col2</div>
+          <div className="hidden sm:block border">col3</div>
+        </div>
+        </div>
+    </>
   )
 }
 
