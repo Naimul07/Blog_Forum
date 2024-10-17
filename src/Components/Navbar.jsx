9d385651f56ba1d28cee17d9cb450a6620cfae2a
Naimul07 <naimul.cse.brur@gmail.com>
2024-10-17 11:44:30 +0600

    Navar structure

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,29 @@
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+      <div className="py-3 fixed top-0 w-full border-b">
+        <nav className="container mx-auto flex justify-between gap-4 ">
+          <div>
+            <h1>BlogForum</h1>
+          </div>
+          <ul className="flex gap-6">
+            <li>
+              <input type="text" className="border rounded-md border-black" />
+            </li>
+            <li>
+              Notification
+            </li>
+            <li>
+              create post
+            </li>
+            <li>
+              user
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   )
 }
 
